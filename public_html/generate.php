16edbc1eedf95718470b5bfae889ac6e3cd78d1f
Udlei Nattis <udlei@protonmail.ch>
2016-01-27 15:43:30 -0200

    Added inputMyPrivateKey

--- public_html/generate.php
+++ public_html/generate.php
@@ -3,15 +3,26 @@
 require_once "Mail.php";
 
 $dn = array(
-    "countryName" => $_POST["inputCountry"],
-    "stateOrProvinceName" => $_POST["inputState"],
-    "localityName" => $_POST["inputLocality"],
-    "organizationName" => $_POST["inputOrganization"],
-    "organizationalUnitName" => $_POST["inputOrganizationalUnit"],
-    "commonName" => $_POST["inputCommonName"]
+  "countryName" => $_POST["inputCountry"],
+  "stateOrProvinceName" => $_POST["inputState"],
+  "localityName" => $_POST["inputLocality"],
+  "organizationName" => $_POST["inputOrganization"],
+  "organizationalUnitName" => $_POST["inputOrganizationalUnit"],
+  "commonName" => $_POST["inputCommonName"]
 );
 
-$privkey = openssl_pkey_new();
+if ($_POST["inputMyPrivateKey"]) {
+
+  $privkey = openssl_pkey_get_private($_POST["inputMyPrivateKey"]);
+
+  if (!$privkey) {
+    print "You have entered an invalid key. Verify and try again.";
+    exit();
+  }
+
+} else {
+  $privkey = openssl_pkey_new();
+}
 
 $csr = openssl_csr_new($dn, $privkey);
 
@@ -26,35 +37,33 @@ print $msg;
 
 if ($_POST["inputYourEmail"]) {
 
-	$contents = file_get_contents("/etc/smtp.conf");
-	$contents = utf8_encode($contents);
-	$results = json_decode($contents);
+  $contents = file_get_contents("/etc/smtp.conf");
+  $contents = utf8_encode($contents);
+  $results = json_decode($contents);
 
-	$from = "CSR Ninja <noreply@csr.ninja>";
-	$to = $_POST["inputYourEmail"];
-	$subject = "Your CSR and KEY - ".$_POST["inputCommonName"];
-	$body = $msg;
+  $from = "CSR Ninja <noreply@csr.ninja>";
+  $to = $_POST["inputYourEmail"];
+  $subject = "Your CSR and KEY - ".$_POST["inputCommonName"];
+  $body = $msg;
 
- 	$headers = array (
-		'From' => $from,
-   		'To' => $to,
-   		'Subject' => $subject
-	);
+  $headers = array (
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+  );
 
  	$smtp = Mail::factory('smtp',array (
-		'host' => $results->host,
-     		'auth' => true,
-     		'username' => $results->username,
-     		'password' => $results->password)
-	);
+    'host' => $results->host,
+    'auth' => true,
+    'username' => $results->username,
+    'password' => $results->password)
+  );
 
- 	$mail = $smtp->send($to, $headers, $body);
+  $mail = $smtp->send($to, $headers, $body);
  
-	if (PEAR::isError($mail)) {
-   		echo("<p>" . $mail->getMessage() . "</p>");
-  	} else {
-#   		echo("<p>Message successfully sent!</p>");
-  	}
+  if (PEAR::isError($mail)) {
+    print $mail->getMessage();
+  }
 
 }
 
--- public_html/index.php
+++ public_html/index.php
@@ -21,77 +21,77 @@
 
     <script type="text/javascript">
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  $("#formCertificate").validate({
-    rules: {
-        "inputCountry": {
-            minlength: 2,
-            maxlength: 2,
-            required: true
-        },
-        "inputState": {
-            required: true
-        },
-        "inputLocality": {
-            required: true
-        },
-        "inputOrganization": {
-            required: true
-        },
-        "inputOrganizationalUnit": {
-            required: true
-        },
-        "inputCommonName": {
-            required: true
-        },
-        "inputYourEmail": {
-            email: true
-        },
+    $("#formCertificate").validate({
+        rules: {
+            "inputCountry": {
+                minlength: 2,
+                maxlength: 2,
+                required: true
+            },
+            "inputState": {
+                required: true
+            },
+            "inputLocality": {
+                required: true
+            },
+            "inputOrganization": {
+                required: true
+            },
+            "inputOrganizationalUnit": {
+                required: true
+            },
+            "inputCommonName": {
+                required: true
+            },
+            "inputYourEmail": {
+                email: true
+            },
 
-	},
-     highlight: function(element) {
-        $(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function(element) {
-        $(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
-    errorPlacement: function(error, element) {
-        if(element.parent('.input-group').length) {
-            error.insertAfter(element.parent());
-        } else {
-            error.insertAfter(element);
+        },
+         highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        submitHandler: function(form) {
+
+            $('#alertEmail').hide();
+
+            $('#certificateResult').show();
+            $('#textareaCSR').val('Please wait...');
+            $(window).scrollTop(($("#certificateResult").offset().top - 10));
+
+            $.post("generate.php", { 
+                    inputCountry: $('#inputCountry').val(), 
+                    inputState: $('#inputState').val(), 
+                    inputLocality: $('#inputLocality').val(), 
+                    inputOrganization: $('#inputOrganization').val(), 
+                    inputOrganizationalUnit: $('#inputOrganizationalUnit').val(), 
+                    inputCommonName: $('#inputCommonName').val(),
+                    inputYourEmail: $('#inputYourEmail').val()
+            }).done(function( data ) {
+
+                if ($('#inputYourEmail').val()) {
+                    $('#alertEmail').html('A copy was sent to your email - ' + $('#inputYourEmail').val());
+                    $('#alertEmail').show();
+                }
+
+                $('#textareaCSR').val(data); 
+
+            }); 
+
         }
-    },
-	submitHandler: function(form) {
-
-		$('#alertEmail').hide();
-
-		$('#certificateResult').show();
-		$('#textareaCSR').val('Please wait...');
-		$(window).scrollTop(($("#certificateResult").offset().top - 10));
-
-        	$.post( "generate.php", { 
-                	inputCountry: $('#inputCountry').val(), 
-                	inputState: $('#inputState').val(), 
-                	inputLocality: $('#inputLocality').val(), 
-                	inputOrganization: $('#inputOrganization').val(), 
-                	inputOrganizationalUnit: $('#inputOrganizationalUnit').val(), 
-                	inputCommonName: $('#inputCommonName').val(),
-			inputYourEmail: $('#inputYourEmail').val()
-        	}).done(function( data ) {
-
-			if ($('#inputYourEmail').val()) {
-				$('#alertEmail').html('A copy was sent to your email - ' + $('#inputYourEmail').val());
-				$('#alertEmail').show();
-			}
-
-                	$('#textareaCSR').val(data); 
-        	}); 
-
-
-    	}
 	
     });
 
@@ -207,6 +207,10 @@ label .help-block {
     margin-bottom: 5px;
 }
 
+textarea.form-control {
+    font-family: Courier, monospace;
+}
+
 #textareaCSR {
 	font-family: Courier, monospace;
 	font-size: 13px;
@@ -217,6 +221,11 @@ label .help-block {
 	margin-top: 40px;
 	position: relative;
 	overflow: hidden;
+}
+
+div.mark-to-use-key { 
+    padding-top: 0px;
+    padding-bottom: 15px;
 }
 
     </style>
@@ -311,9 +320,50 @@ label .help-block {
               <input type="text" class="form-control input-lg" id="inputYourEmail" name="inputYourEmail" placeholder="Optional">
             </div>
           </div>
+            <div class="form-group checkbox">
+              <div class="col-sm-4"></div>
+              <div class="col-sm-8" class="mark-to-use-key">
+                <label><input type="checkbox">Mark to use your KEY to generate CSR</label>
+              </div>
+          </div>
+          <div class="form-group">
+            <label for="inputMyPrivateKey" class="col-sm-4 control-label">Your Private Key
+                <span class="help-block">You can use your Private Key</span>
+            </label>
+            <div class="col-sm-8">
+              <textarea class="form-control input-sm" id="inputMyPrivateKey" name="inputMyPrivateKey" placeholder="-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/mmvu3dioOg8F
+AhBu+XkJMIKYfpN4rA3cyvXiTEZ6H4KWVua7IalGHJulI2Z1LveYw2bWh2hFbxTM
+JMaFJLmt84wfRjqD6WM/oifoqAjg1dxkk2iP0I/FnZkfYNwTx97XrKamxEByldp/
+kSo5HvXPxMND2ukj/oKytEPqSkl/c8FuQoMMK+3EwIT7UxukshdIV/2cuCeSK1O7
+HfbofC9AgfKmvEiyghew+vYg/fOWlc5/sXgQtNcpmoq6XWf1PSjE90YT/iK4/a8A
+2rjDY5jbJeluaMDX4LFk8SOM64/ynm22Eq2cz+nuLYgq7ZtdY+GJbepX0x7WA3Ab
+aon9dVjjAgMBAAECggEAPgqdqnIRZp+uH1wEwDpts8kOcJyD2g40Sus5ZTZ3l7G+
+Ujn/albC/h3jVDy7P3Se3ctf8FVWMS1/2X6K/a/fo/D5zWKrLe2TopY6P/6xKUR2
+oeaNdAv/keivHVb6zDjq8oFIUTW4/KGDZ800k9xvkgBPjhhDZ9FQttbiKuo1FFxy
+hyfupiSON+H6Tbx9OoUHBZ207rcc30rGwdHvieY2V0GwNDtvv/Tlr/T9K6jYE9p7
+FgEcjJpe5HQaF+W++3V9LdzN9xwfcv9l6GbHTzdX93iwhl1YBWSNVOAAbWSHrpxj
+/pBMx1pP51CvAFG+eStK9lDViPxBOhYaQir9UGdpAQKBgQDpE+qTj3oWL77ARMLJ
+Ur76vAQ4s5EsxcCRJDo3hthslvSvGWEdsk1UkuEECzDfMzPAQGNrkTF9jdqvLKdo
+2t1pLhgXtA8csXKPT/pLTLKEAHWmG5r3VRh0aFCPaXriku8quCH0JXX3QngY+uyg
+xYLO9ogWQTFybXALO293aU3igQKBgQDSclH6fPUm94DRtYExv521ElHtzQ5HUAKa
+YvW63yO3HKMwltq9bE/DvyQmXFMDFKlslxnOnZzPZhQy/W7iLBbzDE91W4eJMdcK
+ac1b0JTKUOQcru8/KFKuuob200pmFrYkhSH2sVoHfzMGGiXV33qgppBS81koNlnx
+ghl0a9RBYwKBgQDh5qLBuBrtLANmWH6uH9voKpiw2CGJ0nSsd/9Q2AolXT30A9Yj
+sS/QG7epcqtAeWDcIcv6crQT64wbolzzfMWB7uH8/1ByT+4nxq1hlAmldMiSYUkW
+FUzpbj0+ck5fEWhq8CJAfppSNwMtuu3jeOsqarlFUJR7QmSJDmcfxlltgQKBgCMt
+fuybNLOVzc/AQl9Zg3hAHrfcfhWHl1fNUPwyzpqOS+0UWm4ti8vRyK4s4qdyLbv8
+KhEyoYK9soSvfniKHC/2j9WzPh292g9gjjZZ0HVdPjRZPZ7WE7Qnc+YQfbBA/aNv
+KbU+Aimcpp/PgSGWIL53+UoUaYPiSfin4a9nUuy3AoGAOMUQv1b2tl7E8GB01lTK
+ef0L9oV4+IO0pw+WCLFx9vKIMtdUTzY4py5T5RTqBp3OfW0fzDhilt9g5LwW48GC
+LD27CME9l4YIj7RPaia3yQUfNRm8wdc77YYaT/5BlUGgMZyr+pFIfYliX9Ekg12S
+LkhYm5SGJYi3a7u3uLEE6FE=
+-----END PRIVATE KEY-----" rows="28"></textarea>
+            </div>
+          </div>
           <div class="form-group">
             <div class="col-sm-offset-4 col-sm-8">
-              <button type="submit" class="btn btn-default generate" id="generateCSR">Generate CSR</button>
+              <button type="button" class="btn btn-danger generate" id="generateCSR">Generate KEY and CSR</button>
             </div>
           </div>
         </form>
@@ -326,15 +376,6 @@ label .help-block {
             </div>
         </div>
 
-    </div>
-
-    <div id="footer">
-        <div class="container">
-            <span>CSR.NINJA</span> is 
-            <a href="https://github.com/unattis/csr.ninja">open source</a>, 
-            general feedback and ideas are greatly appreciated via either 
-            the <a href="https://github.com/unattis/csr.ninja/issues">GitHub issues</a>.
-        </div>
     </div>
 
     <a href="https://github.com/udlei/csr.ninja"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>
